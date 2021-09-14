0e32d3abf503b1935bd16681077c677e1eaa3b3b
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 15:29:08 -0300

    Exercício Concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double ml, convkm;
+            Console.Write("Digite a medida em milhas:");
+            ml= double.Parse(Console.ReadLine());
+            convkm= ml * 16009;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{convkm} Km");
+            Console.ResetColor();
+
+        }
+    }
+}
